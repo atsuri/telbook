dc8a9ba939b6b2aee71dd7c695116b01abd3da96
atsuri <ne201094@senshu-u.jp>
2021-11-17 08:59:05 +0000

    FriendとTelephoneバリデーション編集

--- app/models/telephone.rb
+++ app/models/telephone.rb
@@ -6,8 +6,8 @@ class Telephone < ApplicationRecord
             with: /\A[\d\(\)\-]+\z/,
             # \A先頭1文字、\d数字0-9、\エスケープ、＋1文字以上、\z最後の1文字
             allow_blank: true,
-            # message: :invalid_member_name
+            message: :invalid_telephone_number
         },
-        length: { minimum: 8, maximum: 20 },
+        length: { minimum: 8, maximum: 20, message: :invalid_telephone_number },
         uniqueness: { case_sensitive: false }
 end
