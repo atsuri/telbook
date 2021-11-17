1b73a8d4462fcf967fe2fdd5812f90e021792154
atsuri <ne201094@senshu-u.jp>
2021-11-17 08:56:34 +0000

    FriendとTelephoneシードデータ編集

--- db/seeds/development/telephones.rb
+++ db/seeds/development/telephones.rb
@@ -2,7 +2,7 @@ Friend.all.each do |friend|
     %w(33311112222 66677778888).each do |num|
         Telephone.create(
             friend_id: friend.id,
-            number: num
+            number: (num.to_i+friend.id).to_s
         )
     end
 end
